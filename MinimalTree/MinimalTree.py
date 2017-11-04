13f6f8a19d17495293e8f0e987d0f1167bbe52c1
Shuixi Li <shuixili@gmail.com>
2017-11-04 14:31:56 -0400

    minimal tree

--- MinimalTree/MinimalTree.py
+++ MinimalTree/MinimalTree.py
@@ -1,1 +1,29 @@
 __author__ = 'lanceli'
+
+from math import ceil
+
+class BinaryTree(object):
+    def __init__(self, nodeVal):
+        self.nodeVal = nodeVal
+        self.leftNode= None
+        self.rightNode = None
+
+    def getLeftNode(self):
+        return self.leftNode
+
+    def getRightNode(self):
+        return self.rightNode
+
+    def setNodeVal(self, val):
+        self.nodeVal = val
+
+    def getNodeVal(self, val):
+        return self.nodeVal
+
+
+def createBinaryTree(sortedArray):
+    if (len(sortedArray)/2 == 0):
+        return
+        #implement when array is even in size
+    else:
+        middle = sortedArray[math.ceil(len(sortedArray)/2)]
