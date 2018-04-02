9fa05f81347d8322fa43643e87c97dcaeae06ca5
Shuixi Li <shuixili@gmail.com>
2018-04-01 20:39:48 -0400

    more leetcode incompleted

---
+++ LeetCOde/Merge Two Sorted Lists/solx.py
@@ -0,0 +1,30 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        
+
+
+        
+        l3= foo =ListNode(0)
+        
+        while l1 and l2:
+            # print('l1 : ', l1.val, 'l2 : ', l2.val)
+            if l1.val > l2.val:
+                l3.next = l2
+                l2 = l2.next
+            else:
+                l3.next = l1
+                l1 = l1.next
+            l3 = l3.next
+        l3.next = l2 or l1
+        return foo.next
---
+++ LeetCOde/Swap Nodes in Pairs/solx.py
@@ -0,0 +1,31 @@
+# Given a linked list, swap every two adjacent nodes and return its head.
+
+# For example,
+# Given 1->2->3->4, you should return the list as 2->1->4->3.
+
+# Your algorithm should use only constant space. You may not modify the values in the list, only nodes itself can be changed.
+
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution(object):
+    def swapPairs(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+
+        curr = dummy = ListNode(0)
+
+        dummy.next = head
+
+        while curr.next and curr.next.next:
+            temp = curr.next.next
+            curr.next.next = curr
+            curr.next = temp
+            curr = temp
+
+        return dummy.next
