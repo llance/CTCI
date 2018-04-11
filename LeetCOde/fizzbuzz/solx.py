9e3767c0fbda6565d3e25358b0b0707f69d20c3a
Shuixi Li <shuixili@gmail.com>
2018-04-10 23:47:56 -0400

    fizzbuzz

---
+++ LeetCOde/fizzbuzz/solx.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def __init__(self):
+        self.answer = list()
+    def fizzBuzz(self, input):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        for n in range(1, input+1):
+            print('n is : ', n)
+            if n % 3 == 0 and n % 5 == 0:
+                self.answer.append("FizzBuzz")
+            elif n % 3 == 0:
+                self.answer.append("Fizz")
+            elif n% 5 == 0:
+                self.answer.append("Buzz")
+            else:
+                self.answer.append(str(n))
+        return self.answer
